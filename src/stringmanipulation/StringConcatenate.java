efac1a3b7f27ac292275ce99e76e787cba561e4f
Kgotso <peacematjato@gmail.com>
2022-10-16 17:59:14 +0200

    StringConcatenate added

--- src/stringmanipulation/StringConcatenate.java
+++ src/stringmanipulation/StringConcatenate.java
@@ -1,5 +1,7 @@
 package stringmanipulation;
 
+import java.util.Scanner;
+
 /**
  * A Java Program that concatenates a given string to the end of another string
  *
@@ -10,8 +12,22 @@ public class StringConcatenate
 {
     public static void main(String[] args) {
 
+        //creating my scanner object that will read the input given by the user
+        Scanner sc = new Scanner(System.in);
+
         String defaultString = "Java is a very special programming language";
+        String programmingChoice = "";
 
         System.out.println(defaultString.concat(" which I would recommend to other programmers"));
+        System.out.println();
+
+        System.out.println("What is your favourite programming language between (Java, Python and C#)");
+        programmingChoice = sc.next();
+
+        if(programmingChoice.equals("Java"))
+            System.out.println(defaultString.concat(" is a popular programming language, created in 1995. " +
+                "It is owned by Oracle, and more than 3 billion devices run Java."));
+        else if(programmingChoice.equals("Java"))
+
     }
 }
